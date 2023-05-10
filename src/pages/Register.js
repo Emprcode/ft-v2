12b383042023ad6312217d58fb818c4a94e74015
Narayan Aryal <aryal.coding@gmail.com>
2023-05-10 13:22:59 +1000

    register page creating

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,18 +1,65 @@
-import React from 'react'
-import { MainLayout } from '../components/layout/MainLayout'
-import {Row, Col, Container} from 'react-bootstrap'
+import React from "react";
+import { MainLayout } from "../components/layout/MainLayout";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { FormComponents } from "../components/formComponents/FormComponents";
 
 const Register = () => {
+  const inputs = [
+    {
+      name: "username",
+      type: "text",
+      label: "User Name",
+      placeholder: "Jack",
+      required: true,
+    },
+    {
+      email: "email",
+      type: "email",
+      label: "Email",
+      placeholder: "Jack@gmail.com",
+      required: true,
+    },
+    {
+      name: "password",
+      type: "password",
+      label: "Password",
+      placeholder: "*****",
+      required: true,
+    },
+    {
+      name: "confirmPassword",
+      type: "password",
+      label: "Confirm Password",
+      placeholder: "*****",
+      required: true,
+    },
+  ];
   return (
     <MainLayout>
-        <Container>
-        <Row>
-            <Col className='info'> info</Col>
-            <Col className=''> form</Col>
-        </Row>
-        </Container>
-    </MainLayout>
-  )
-}
+      <Container>
+        <Row className="mt-3">
+          <h4 className="text-center"> Registration</h4>
+          <hr />
+          <Col className="info ">
+            <h4> Welcome!</h4>
 
-export default Register
+            <p> Please register here to use our system </p>
+          </Col>
+          <Col className="">
+            <Form>
+              {inputs.map((item, i) => (
+                <FormComponents key={i} {...item} />
+              ))}
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </MainLayout>
+  );
+};
+
+export default Register;
